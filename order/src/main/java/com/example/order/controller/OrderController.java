3f38b33d93380e308282ea0e640391e1708687f6
Pamindu Virunjith <paminduvirunjith2002@gmail.com>
2025-11-06 17:38:41 +0530

    Asynchronous communication (event driven architecture) with apache kafka

--- order/src/main/java/com/example/order/controller/OrderController.java
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.example.order.controller;
 
+import com.example.base.dto.OrderEventDTO;
 import com.example.order.common.OrderResponse;
 import com.example.order.dto.OrderDTO;
+import com.example.order.kafka.OrderProducer;
 import com.example.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,9 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private OrderProducer orderProducer;
+
     @GetMapping("/getorders")
     public List<OrderDTO> getOrders(){
         return orderService.getAllOrders();
@@ -28,6 +33,11 @@ public class OrderController {
 
     @PostMapping("/addorder")
     public OrderResponse addOrders(@RequestBody OrderDTO orderDTO){
+        OrderEventDTO orderEventDTO = new OrderEventDTO();
+
+        orderEventDTO.setMessage("Order is ready");
+        orderEventDTO.setStatus("Pending");
+        orderProducer.sendMessage(orderEventDTO);
         return orderService.saveOrder(orderDTO);
     }
 
